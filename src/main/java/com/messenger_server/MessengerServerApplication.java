d8f2d437ea4aafeb00d249cb2fed933e24140b27
Colin1783 <166104456+Colin1783@users.noreply.github.com>
2024-07-23 11:35:54 +0900

    Initial commit

---
+++ src/main/java/com/messenger_server/MessengerServerApplication.java
@@ -0,0 +1,13 @@
+package com.messenger_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerServerApplication.class, args);
+	}
+
+}
